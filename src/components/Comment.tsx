c404e4499d13891d6f96ac9f073c43544d74cfc4
caiogomes <caio2011gs@gmail.com>
2024-02-23 13:19:11 -0300

    First Commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,58 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://avatars.githubusercontent.com/u/85319481?v=4"
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Caio Gomes</strong>
+              <time
+                title="15 de Dezembro às 11:00"
+                dateTime="2023-12-15 11:00:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
